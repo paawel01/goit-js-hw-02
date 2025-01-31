0a930d6be1289bd691580f96391574e308b16849
paawel01 <pawelzylowski.01@gmail.com>
2025-01-31 18:44:22 +0100

    update
    structure changes

--- js/task-2.js
+++
@@ -1,7 +0,0 @@
-function formatMessage(message, maxLength) {
-    if (message <= maxLength) {
-        return message;
-    } else {
-        return message.slice(0, maxLength) + "...";
-    }
-}
--- task-1.js
+++ task-1.js
@@ -6,11 +6,9 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
     } else {
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     }
-
 }
-
 console.log(makeTransaction(5, 3000, 23000)); 
 console.log(makeTransaction(3, 1000, 15000)); 
 console.log(makeTransaction(10, 5000, 8000)); 
 console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000)); 
+console.log(makeTransaction(10, 500, 5000));
